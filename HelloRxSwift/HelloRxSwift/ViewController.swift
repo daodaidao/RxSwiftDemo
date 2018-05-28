0015f146db2ab254845a2d09cedf474064108877
JayShang <yijieshang@gmail.com>
2018-05-28 16:29:43 +0800

    我的第一个 RxSwift应用程序 -输入验证 准备开撸

---
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloRxSwift
+//
+//  Created by caihongguang on 2018/5/28.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//https://beeth0ven.github.io/RxSwift-Chinese-Documentation/content/first_app.html
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    //我的第一个 RxSwift应用程序 -输入验证
+    
+        
+        
+        
+    }
+
+    
+
+
+}
+
