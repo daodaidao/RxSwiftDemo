0fdf7dab79307f8907cf609c23a41dac74007d56
JayShang <yijieshang@gmail.com>
2018-05-29 09:18:19 +0800

    页面搭建

--- HelloRxSwift/HelloRxSwift/ViewController.swift
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -7,14 +7,30 @@
 //https://beeth0ven.github.io/RxSwift-Chinese-Documentation/content/first_app.html
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+fileprivate let minimalUsernameLength = 5
+fileprivate let minimalPasswordLength = 5
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var usernameOutlet: UITextField!
+    @IBOutlet weak var usernameValidOutlet: UILabel!
+    
+    @IBOutlet weak var passwordOutlet: UITextField!
+    @IBOutlet weak var passwordValidOutlet: UILabel!
+    
+    @IBOutlet weak var doSomethingOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     //我的第一个 RxSwift应用程序 -输入验证
     
+        
+        
         
         
         
