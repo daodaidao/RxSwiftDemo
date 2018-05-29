0f8bdcb4d1949ef6ad81c3bfcd4e9a5aa954117c
JayShang <yijieshang@gmail.com>
2018-05-29 10:25:45 +0800

    第3条 以后就是偏 手册和理论基础了，没事的时候看看 撸一撸

---
+++ 001-RxSwiftDemo/001-RxSwiftDemo/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  001-RxSwiftDemo
+//
+//  Created by caihongguang on 2018/5/29.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class Student: NSObject {
+
+}
---
+++ 001-RxSwiftDemo/001-RxSwiftDemo/ViewController.swift
@@ -0,0 +1,35 @@
+//https://www.jianshu.com/p/065ba28c938a
+//  ViewController.swift
+//  001-RxSwiftDemo
+//
+//  Created by caihongguang on 2018/5/29.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        [🐂,🍠,🐤,🌽].map(cook)
+        
+        //1.函数式编程是种编程范式，它需要我们将函数作为参数传递，或者作为返回值返还。我们可以通过组合不同的函数来得到想要的结果。
+//        let allStudents: [Student] = getSchoolSttudents()
+        
+//        https://beeth0ven.github.io/RxSwift-Chinese-Documentation/content/think_reactive.html
+        
+        // 第3条 以后就是偏 手册和理论基础了，没事的时候看看 撸一撸
+        
+    }
+
+//    func getSchoolSttudents() {
+//
+//    }
+   
+
+}
+
