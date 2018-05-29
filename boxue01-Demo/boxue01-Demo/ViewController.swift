e4da8ecdbe20e71b14cc9c1121843b863a8ba0ab
JayShang <yijieshang@gmail.com>
2018-05-29 11:29:57 +0800

    伯学rxswift demo

---
+++ boxue01-Demo/boxue01-Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  boxue01-Demo
+//
+//  Created by caihongguang on 2018/5/29.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let stringArray = ["1","2","3","4","5","6","7"]
+        
+//        stringArray.flatMap { Int($0)
+//            }.filter( $0 % 2 == 0)
+        
+    }
+
+  
+
+}
+
---
+++ boxue01-Demo/boxue01-DemoTests/boxue01_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  boxue01_DemoTests.swift
+//  boxue01-DemoTests
+//
+//  Created by caihongguang on 2018/5/29.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import XCTest
+@testable import boxue01_Demo
+
+class boxue01_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
